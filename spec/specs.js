ccec81ad958047e3b0c61fdc6fb80f25248d3701
Yvonne <ypeng.tw@gmail.com>
2015-09-25 14:15:01 -0700

    Modify the tests according to the constructor changes

--- spec/specs.js
+++ spec/specs.js
@@ -1,27 +1,37 @@
 describe("Pizza", function() {
-	it("creates a new pizza with the given size and topppings choices ", function() {
-		var myPizza = new Pizza("Small 10\"", ["Pepperoni", "Pineapple", "Spinach"]);
+	it("creates a new pizza with the given size", function() {
+		var myPizza = new Pizza("Small 10\"");
 		expect(myPizza.size).to.equal("Small 10\"");
+	});
+});
+
+describe("Pizza", function() {
+	it("creates a new pizza with the given size and topppings choices ", function() {
+		var myPizza = new Pizza("Small 10\"");
+		var myTopppings = ["Pepperoni", "Pineapple", "Spinach"];
+		myPizza.topppings = myTopppings;
 		expect(myPizza.topppings).to.eql(["Pepperoni", "Pineapple", "Spinach"]);
 	});
 });
+
 describe("Pizza.sizeIndex()", function() {
 	it("adds the sizeIndex method to Pizza", function() {
-	    var myPizza = new Pizza("Medium 12\"", ["Pepperoni", "Pineapple"]);
+	    var myPizza = new Pizza("Medium 12\"");
 	    expect(myPizza.sizeIndex()).to.equal(1);
 	});
 });
 
 describe("Pizza.Price()", function() {
 	it("adds the Price method to Pizza", function() {
-			var myPizza = new Pizza("Large 14\"", ["Sausage", "Pineapple"]);
+			var myPizza = new Pizza("Large 14\"");
+			myPizza.topppings = ["Sausage", "Pineapple"];
 			expect(myPizza.Price()).to.equal(13.2);
 	});
 });
 
 describe("Order", function() {
 	it("creates a new Order with the given quantity and pizza ", function() {
-		var myPizza = new Pizza("Medium 12\"", ["Pepperoni", "Pineapple"]);
+		var myPizza = new Pizza("Medium 12\"");
 		var myOrder = new Order(1, myPizza);
 		expect(myOrder.quantity).to.equal(1);
 		expect(myOrder.pizza).to.equal(myPizza);
@@ -30,7 +40,8 @@ describe("Order", function() {
 
 describe("Order.Total()", function() {
 	it("adds the Total method to Order", function() {
-			var myPizza = new Pizza("Large 14\"", ["Sausage", "Pineapple"]);
+			var myPizza = new Pizza("Large 14\"");
+			myPizza.topppings = ["Sausage", "Pineapple"];
 			var myOrder = new Order(2, myPizza);
 			expect(myOrder.Total()).to.equal(26.4);
 	});
