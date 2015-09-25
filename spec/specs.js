636b98fb578fce7fea1bef1c7db5e7a0d8f49118
Yvonne <ypeng.tw@gmail.com>
2015-09-25 13:23:21 -0700

    Pass tast for Order functions. It will help me to debug the methods

--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,4 @@
-describe('Pizza', function() {
+describe("Pizza", function() {
 	it("creates a new pizza with the given size and topppings choices ", function() {
 		var myPizza = new Pizza("Small 10\"", ["Pepperoni", "Pineapple", "Spinach"]);
 		expect(myPizza.size).to.equal("Small 10\"");
@@ -13,5 +13,20 @@ describe('Pizza', function() {
 	it("adds the Price method to Pizza", function() {
 			var myPizza = new Pizza("Large 14\"", ["Sausage", "Pineapple"]);
 			expect(myPizza.Price()).to.equal(13.2);
+	});
+});
+
+describe("Order", function() {
+	it("creates a new Order with the given quantity and pizza ", function() {
+		var myPizza = new Pizza("Medium 12\"", ["Pepperoni", "Pineapple"]);
+		var myOrder = new Order(1, myPizza);
+		expect(myOrder.quantity).to.equal(1);
+		expect(myOrder.pizza).to.equal(myPizza);
+	});
+
+	it("adds the Total method to Order", function() {
+			var myPizza = new Pizza("Large 14\"", ["Sausage", "Pineapple"]);
+			var myOrder = new Order(2, myPizza);
+			expect(myOrder.Total()).to.equal(26.4);
 	});
 });
